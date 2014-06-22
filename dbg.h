bbbf552192c7844c4b3f6014499583db2d4ea144
Joel Friedly <joelfriedly@gmail.com>
2014-06-22 03:48:40 -0700

    Add logging, top-level expressions, min, max, ^

---
+++ dbg.h
@@ -0,0 +1,57 @@
+/*
+ * Logging macros from Zed Shaw's Learn C the Hard Way
+ */
+#ifndef __dbg_h__
+#define __dbg_h__
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+
+#define clean_errno() (errno == 0 ? "None" : strerror(errno))
+
+
+#if DEBUG
+/* ##__VA_ARGS__ tells the preprocessor to substitute in the extra arguments
+ * ("...")
+ * NOTE(jfriedly):  Some C compilers, such as cpp, don't support this.
+ */
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+/* Use check_debg for common errors that shouldn't typically be reported to the
+ * user, but still need to be handled.
+ */
+#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; }
+#else
+#define debug(M, ...)
+#endif
+
+
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+
+#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+
+
+#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+/* Use check to check  a condition, log, and cleans up if the condition is
+ * false.
+ */
+#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+
+/* Put sentinel in places that execution should never reach, like the default
+ * case for a switch statement.
+ */
+#define sentinel(M, ...) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+
+/* Use check_mem to check if pointers are valid.  Example::
+ * 
+ *     char *block = NULL;
+ *     block = malloc(4096);
+ *     check_mem(block);
+ */
+#define check_mem(A) check((A), "Out of memory.")
+
+
+#endif
--- parsing.c
+++ parsing.c
@@ -1,7 +1,12 @@
+#include <errno.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include "mpc.h"
+
+#define DEBUG 0
+#include "dbg.h"
 
 /* If we are compiling on Windws, compile these functions */
 #ifdef _WIN32
@@ -36,24 +41,30 @@ void add_history(char *unused) {}
 
 long eval_op(long x, char *op, long y)
 {
-	switch (*op) {
-	case '+':
+	if (strcmp(op, "+") == 0)
 		return x + y;
-	case '-':
+	else if (strcmp(op, "-") == 0)
 		return x - y;
-	case '*':
+	else if (strcmp(op, "*") == 0)
 		return x * y;
-	case '/':
+	else if (strcmp(op, "/") == 0)
 		return x / y;
-	case '%':
+	else if (strcmp(op, "%") == 0)
 		return x % y;
-	default:
-		printf("ERROR:  unrecognized operator:  %c", *op);
-		return 0;
-	}
+	else if (strcmp(op, "^") == 0)
+		return pow(x, y);
+	else if (strcmp(op, "min") == 0)
+		return fmin(x, y);
+	else if (strcmp(op, "max") == 0)
+		return fmax(x, y);
+	else
+		sentinel("ERROR:  unrecognized operator:  '%c'", *op);
+
+error:
+	return 0;
 }
 
-long eval(mpc_ast_t * ast)
+long eval(mpc_ast_t *ast)
 {
 	/* If it's tagged as a number, parse out a long and return it */
 	/* TODO(jfriedly):  Support floats! */
@@ -76,16 +87,25 @@ long eval(mpc_ast_t * ast)
 	return x;
 }
 
+long eval_line(mpc_ast_t *ast)
+{
+	/* Ignore the regexes ^ and $ */
+	mpc_ast_t *expr = ast->children[1];
+	return eval(expr);
+}
+
 /* Attempt to parse the user input */
 mpc_ast_t *parse(mpc_parser_t *parser, char *input)
 {
 	mpc_result_t r;
+	debug("Parsing...");
 	if (!mpc_parse("<stdin>", input, parser, &r)) {
-		/* Failure:  print the error */
+		log_err("Failed to parse expression:");
 		mpc_err_print(r.error);
 		mpc_err_delete(r.error);
 		return NULL;
 	}
+	debug("Parsed into an AST successfully.");
 	return r.output;
 }
 
@@ -99,16 +119,20 @@ int main(int argc, char **argv)
 	mpc_parser_t *MyLisp   = mpc_new("mylisp");
 	/* Define the parsers with the followning language */
 	mpca_lang(MPCA_LANG_DEFAULT,
-		"							\
-		digits   : /-?[0-9]+/ ;					\
-		number   : <digits> '.' <digits> | <digits> ;		\
-		operator : '+' | '-' | '*' | '/' | '%' ;		\
-		expr     : '(' <operator> <expr>+ ')' | <number> ;	\
-		mylisp   : /^/ <operator> <expr>+ /$/ ;",
+		"										\
+		digits   : /-?[0-9]+/ ;								\
+		number   : <digits> '.' <digits> | <digits> ;					\
+		operator : '+' | '-' | '*' | '/' | '%' | '^' | \"min\" | \"max\" ;		\
+		expr     : '(' <operator> <expr>+ ')' | <number> ;				\
+		mylisp   : /^/ <expr> /$/ ;",
 		Digits, Number, Operator, Expr, MyLisp);
 
 	puts("My-lisp Version 0.0.0.0.1");
 	puts("Press Ctrl+C to exit\n");
+
+	/* TODO(jfriedly):  Why does initializing readline set ENOENT? */
+	rl_initialize();
+	errno = 0;
 
 	while (1) {
 		char *input = readline("my-lisp> ");
@@ -116,7 +140,7 @@ int main(int argc, char **argv)
 
 		mpc_ast_t *ast = parse(MyLisp, input);
 		if (ast != NULL) {
-			printf("%ld\n", eval(ast));
+			printf("%ld\n", eval_line(ast));
 			mpc_ast_delete(ast);
 		}
 
